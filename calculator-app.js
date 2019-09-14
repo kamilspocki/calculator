22085a249c378e09ba57f9337267cb8efe66009e
Kamil Wrzochalski <49152075+kamilspocki@users.noreply.github.com>
2019-09-14 09:21:53 +0200

    Update calculator-app.js

--- calculator-app.js
+++ calculator-app.js
@@ -1,81 +1,58 @@
-//STR TO INT FROM UI
 var digits = "";
-
-//THE OP + NUMS (from digits)
 var math = [];
-
-//THE RESULT
 var res = 0;
-
-//SCREEN
 var screen;
 screen = document.getElementById('screen');
 
-
-
-/** CALCULATOR ALGO */
-
-//CREATE A NUMBER FROM DIGITS[str]
 function getValue(val) {
     digits += val;
-    screen.innerHTML = digits;
+    screen.innerText = digits;
 }
-//TAKES CARE OF THE CHRONOLOGY and RETURNS THE RESULT
+
 function opChrono(arr, id) {
-    //LAST NUMBER EXCEPTION
+
     if (digits) {
         math.push(parseFloat(digits));
         digits = "";
     }
     arr.map(function(val, idx, arr) {
-        //MULTI FIRST
-        if (val == "*") {
-            arr.splice((idx - 1), 3, (arr[idx - 1] * arr[idx + 1]));
-        }
-        //THEN DIVISION
-        if (arr.indexOf("*") == -1 && val == "/") {
+        
+        if (val == "/") {
             arr.splice((idx - 1), 3, (arr[idx - 1] / arr[idx + 1]));
         }
-    })
+        if (arr.indexOf("/") == -1 && val == "*") {
+            arr.splice((idx - 1), 3, (arr[idx - 1] * arr[idx + 1]));
+        }
+    });
 
-    //no MULTI/DIV
     if (arr.indexOf("*") == -1 && arr.indexOf("/") == -1) {
 
-        //FIRST NUMBER EXCEPTION
         res += arr[0];
 
-        //GEN RES
         arr.map(function(value, index, arr) {
             if (value == "-" || value == "+") {
                 res += parseFloat(value + arr[index + 1]);
             }
-        })
-
-        //SHOW RES
-        screen.innerHTML = res;
-        //RECU STOP
+        });
+        screen.innerText = res;
         return arr;
     };
-    //RECU GO
     return opChrono(arr, id);
 }
 
 function getSign(id) {
-    //create INT and put into OP ARR
-    math.push(parseFloat(digits));
-    //the SIGN
-    lastSign = document.getElementById(id).id;
-    if (lastSign != "eq") {
-        math.push(lastSign);
+
+    if (digits) {
+        math.push(parseFloat(digits));
+        lastSign = document.getElementById(id).id;
+
+        if (lastSign != "eq") {
+            math.push(lastSign);
+        }
     }
-    //show
-    screen.innerHTML = math.join(" ");
-    //reset to create a new NUMBER
+    screen.innerText = math.join(" ");
     digits = "";
 }
-
-//OTHER FUNCTIONS
-//clear
 
 document.getElementById("AC").addEventListener('click', function() {
 
@@ -83,7 +60,7 @@ document.getElementById("AC").addEventListener('click', function() {
     digits = "";
     res = 0;
 
-    screen.innerHTML = 0;
+    screen.innerText = 0;
 
     return math, digits, res;
 });
